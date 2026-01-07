d3513ccbfcd163ef5653c3d85bdef39de9ae2854
LuuckA21 <luca.dalu@outlook.it>
2026-01-07 01:08:42 +0100

    Fix PillarLog

--- src/main/java/me/luucka/pillars/util/PillarLog.java
+++ src/main/java/me/luucka/pillars/util/PillarLog.java
@@ -10,41 +10,41 @@ import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class PillarLog {
 
-    private static final ComponentLogger LOGGER = ComponentLogger.logger(PillarPlugin.class);
+	private static final ComponentLogger LOGGER = ComponentLogger.logger(PillarPlugin.getInstance().getName());
 
-    public static void info(final String... messages) {
-        for (final String message : messages) {
-            if (isValidMessage(message)) {
-                LOGGER.info(Component.text(message));
-            }
-        }
-    }
+	public static void info(final String... messages) {
+		for (final String message : messages) {
+			if (isValidMessage(message)) {
+				LOGGER.info(Component.text(message));
+			}
+		}
+	}
 
-    public static void warn(final String... messages) {
-        for (final String message : messages) {
-            if (isValidMessage(message)) {
-                LOGGER.warn(Component.text(message).color(NamedTextColor.YELLOW));
-            }
-        }
-    }
+	public static void warn(final String... messages) {
+		for (final String message : messages) {
+			if (isValidMessage(message)) {
+				LOGGER.warn(Component.text(message).color(NamedTextColor.YELLOW));
+			}
+		}
+	}
 
-    public static void error(final String... messages) {
-        for (final String message : messages) {
-            if (isValidMessage(message)) {
-                LOGGER.error(Component.text(message).color(NamedTextColor.RED));
-            }
-        }
-    }
+	public static void error(final String... messages) {
+		for (final String message : messages) {
+			if (isValidMessage(message)) {
+				LOGGER.error(Component.text(message).color(NamedTextColor.RED));
+			}
+		}
+	}
 
-    public static void debug(final String... messages) {
-        for (final String message : messages) {
-            if (isValidMessage(message)) {
-                LOGGER.debug(Component.text(message).color(NamedTextColor.GRAY));
-            }
-        }
-    }
+	public static void debug(final String... messages) {
+		for (final String message : messages) {
+			if (isValidMessage(message)) {
+				LOGGER.debug(Component.text(message).color(NamedTextColor.GRAY));
+			}
+		}
+	}
 
-    private static boolean isValidMessage(final String message) {
-        return message != null && !"none".equals(message) && !message.isBlank();
-    }
+	private static boolean isValidMessage(final String message) {
+		return message != null && !"none".equals(message) && !message.isBlank();
+	}
 }
